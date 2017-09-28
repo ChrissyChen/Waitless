01a2198c27e13a0b7ab3c7f3eb9a9a09e2dce15e
Chrissy <xchen4@mail.sfsu.edu>
2017-09-28 00:26:01 -0700

    add confirm popup window after getting a number

--- app/src/main/java/project/csc895/sfsu/waitless/ui/GetNumberActivity.java
+++ app/src/main/java/project/csc895/sfsu/waitless/ui/GetNumberActivity.java
@@ -1,23 +1,27 @@
 package project.csc895.sfsu.waitless.ui;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.TextView;
 
 import project.csc895.sfsu.waitless.R;
-import project.csc895.sfsu.waitless.model.Restaurant;
 
 public class GetNumberActivity extends AppCompatActivity {
 
     private static final String TAG = "GetNumber Activity";
+    private LinearLayout mLinearLayout;
     private EditText mFirstNameField, mLastNameField, mTelephoneField, mPartyNumberField;
     private Button mConfirmButton;
-    private PopupWindow popUpWindow;
+    private PopupWindow mPopupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,7 @@ public class GetNumberActivity extends AppCompatActivity {
 
         overridePendingTransition(R.anim.trans_left_in, R.anim.trans_left_out);
 
+        mLinearLayout = (LinearLayout) findViewById(R.id.getNumberLinearLayout);
         mFirstNameField = (EditText) findViewById(R.id.firstName);
         mLastNameField = (EditText) findViewById(R.id.lastName);
         mTelephoneField = (EditText) findViewById(R.id.telephone);
@@ -35,10 +40,30 @@ public class GetNumberActivity extends AppCompatActivity {
         mConfirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                showConfirmPopupWindow();
             }
         });
     }
+
+    public void showConfirmPopupWindow() {
+        LayoutInflater inflater = (LayoutInflater) GetNumberActivity.this.
+                                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View customView = inflater.inflate(R.layout.popup_window_number, null);
+        mPopupWindow = new PopupWindow(customView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        mPopupWindow.showAtLocation(mLinearLayout, Gravity.CENTER, 0, 0);
+//        if(Build.VERSION.SDK_INT >= 21){
+//            mPopupWindow.setElevation(5.0f);
+//        }
+
+        Button closeButton = (Button) customView.findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPopupWindow.dismiss();
+            }
+        });
+    }
+
 
     @Override
     public void onBackPressed() {
