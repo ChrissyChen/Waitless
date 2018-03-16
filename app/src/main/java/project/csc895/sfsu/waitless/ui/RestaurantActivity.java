d5e65f14d6ca1171db0af01084b6ec7d99fb9aa6
Chrissy <xchen4@mail.sfsu.edu>
2018-03-16 16:18:01 -0700

    load waitNums on restaurant details page

--- app/src/main/java/project/csc895/sfsu/waitless/ui/RestaurantActivity.java
+++ app/src/main/java/project/csc895/sfsu/waitless/ui/RestaurantActivity.java
@@ -16,20 +16,26 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import project.csc895.sfsu.waitless.R;
 import project.csc895.sfsu.waitless.model.Restaurant;
+import project.csc895.sfsu.waitless.model.Waitlist;
 
 public class RestaurantActivity extends AppCompatActivity {
 
     private static final String TAG = "Restaurant Activity";
     private static final String RESTAURANT_CHILD = "restaurants";
     public static final String EXTRA_RESTAURANT_NAME = "Pass Restaurant Name";
+    private static final String WAITLIST_CHILD = "waitlists";
+    private static final String RESTAURANT_ID_CHILD = "restaurantID";
     private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-    private String restaurantID, restaurantName;
+    private String restaurantID, restaurantName, waitlistID;
     private ImageView mRestaurantImage;
-    private TextView mRestaurantName, mCuisines, mOperateStatus, mRestaurantAddress, mRestaurantPhone;
+    private TextView mRestaurantName, mCuisines, mOperateStatus, mRestaurantAddress, mRestaurantPhone,
+                    mTableAWaitingParty, mTableBWaitingParty, mTableCWaitingParty, mTableDWaitingParty,
+                    mTableAEstimateTime, mTableBEstimateTime, mTableCEstimateTime, mTableDEstimateTime;
     private Button mGetNumberButton;
 
     @Override
@@ -46,14 +52,9 @@ public class RestaurantActivity extends AppCompatActivity {
         restaurantID = intent.getStringExtra(SearchActivity.EXTRA_RESTAURANT_ID);
         Log.d(TAG + " ID:", restaurantID);
 
-        mRestaurantImage = (ImageView) findViewById(R.id.restaurantImageView);
-        mRestaurantName = (TextView) findViewById(R.id.name);
-        mCuisines = (TextView) findViewById(R.id.cuisine);
-        mOperateStatus = (TextView) findViewById(R.id.operateStatusTextView);
-        mRestaurantAddress = (TextView) findViewById(R.id.restaurantAddress);
-        mRestaurantPhone = (TextView) findViewById(R.id.restaurantTelephone);
-
+        initViews();
         loadRestaurantValue();
+        loadWaitlistInfo();
 
         mGetNumberButton = (Button) findViewById(R.id.getNumberButton);
         mGetNumberButton.setOnClickListener(new View.OnClickListener() {
@@ -66,6 +67,24 @@ public class RestaurantActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void initViews() {
+        mRestaurantImage = (ImageView) findViewById(R.id.restaurantImageView);
+        mRestaurantName = (TextView) findViewById(R.id.name);
+        mCuisines = (TextView) findViewById(R.id.cuisine);
+        mOperateStatus = (TextView) findViewById(R.id.operateStatusTextView);
+        mRestaurantAddress = (TextView) findViewById(R.id.restaurantAddress);
+        mRestaurantPhone = (TextView) findViewById(R.id.restaurantTelephone);
+
+        mTableAWaitingParty = (TextView) findViewById(R.id.tableAWaitingParty);
+        mTableBWaitingParty = (TextView) findViewById(R.id.tableBWaitingParty);
+        mTableCWaitingParty = (TextView) findViewById(R.id.tableCWaitingParty);
+        mTableDWaitingParty = (TextView) findViewById(R.id.tableDWaitingParty);
+        mTableAEstimateTime = (TextView) findViewById(R.id.tableAEstimateTime);
+        mTableBEstimateTime = (TextView) findViewById(R.id.tableBEstimateTime);
+        mTableCEstimateTime = (TextView) findViewById(R.id.tableCEstimateTime);
+        mTableDEstimateTime = (TextView) findViewById(R.id.tableDEstimateTime);
     }
 
     private void loadRestaurantValue() {
@@ -96,6 +115,53 @@ public class RestaurantActivity extends AppCompatActivity {
         });
     }
 
+    private void loadWaitlistInfo() {
+        Query query = mDatabase.child(WAITLIST_CHILD)
+                .orderByChild(RESTAURANT_ID_CHILD)
+                .equalTo(restaurantID);
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.hasChildren()) {
+                    Log.d("Error", "NO WAITLIST SHOWS");
+                } else {
+                    for (DataSnapshot objSnapshot: dataSnapshot.getChildren()) {
+                        Waitlist waitlist = objSnapshot.getValue(Waitlist.class);
+                        if (waitlist != null) {
+                            waitlistID = waitlist.getWaitlistID();
+                            Log.d("waitlistID inside", waitlistID);
+
+                            int waitNumTableA = waitlist.getWaitNumTableA();
+                            int waitNumTableB = waitlist.getWaitNumTableB();
+                            int waitNumTableC = waitlist.getWaitNumTableC();
+                            int waitNumTableD = waitlist.getWaitNumTableD();
+
+                            mTableAWaitingParty.setText(String.valueOf(waitNumTableA));
+                            mTableBWaitingParty.setText(String.valueOf(waitNumTableB));
+                            mTableCWaitingParty.setText(String.valueOf(waitNumTableC));
+                            mTableDWaitingParty.setText(String.valueOf(waitNumTableD));
+
+                            String estimateTimeTableA = waitNumTableA * 5 + " min";
+                            String estimateTimeTableB = waitNumTableB * 5 + " min";
+                            String estimateTimeTableC = waitNumTableC * 5 + " min";
+                            String estimateTimeTableD = waitNumTableD * 5 + " min";
+
+                            mTableAEstimateTime.setText(estimateTimeTableA);
+                            mTableBEstimateTime.setText(estimateTimeTableB);
+                            mTableCEstimateTime.setText(estimateTimeTableC);
+                            mTableDEstimateTime.setText(estimateTimeTableD );
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 
     @Override
     public void onBackPressed() {
